b5ace72f88ebad775806d2a1513afc992c373019
Ashish151102 <ashishkumyadav789@gmail.com>
2025-03-04 12:43:38 +0530

    [Ashish] Add . Implemented functionality to calculate total wages for a month.

--- src/main/javascript/com.bridgelabz/EmployeeWage.js
+++ src/main/javascript/com.bridgelabz/EmployeeWage.js
@@ -4,46 +4,27 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const WORKING_DAYS_PER_MONTH = 20;
 
-// Generating work type :- Absent, Part-Time, Full-Time
-let empCheck = Math.floor(Math.random() * 3);
-
-let empHours = 0;
-
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHours = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:const IS_ABSENT = 0;
-                      const IS_PART_TIME = 1;
-                      const IS_FULL_TIME = 2;
-                      const PART_TIME_HOURS = 4;
-                      const FULL_TIME_HOURS = 8;
-                      const WAGE_PER_HOUR = 20;
-
-                      // Function to get work hours
-                      function getWorkHours(empCheck) {
-                          switch (empCheck) {
-                              case IS_PART_TIME:
-                                  return PART_TIME_HOURS;
-                              case IS_FULL_TIME:
-                                  return FULL_TIME_HOURS;
-                              default:
-                                  return 0;
-                          }
-                      }
-
-                      // Generating work type :- Absent, Part-Time, Full-Time
-                      let empCheck = Math.floor(Math.random() * 3);
-                      let empHours = getWorkHours(empCheck);
-                      let dailyWage = empHours * WAGE_PER_HOUR;
-
-                      console.log(`Employee worked ${empHours} hours, Daily Wage: $${dailyWage}`);
-        empHours = FULL_TIME_HOURS;
-        break;
-    default:
-        empHours = 0;
+// Function to get work hours
+function getWorkHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
-let dailyWage = empHours * WAGE_PER_HOUR;
-console.log(`Employee worked ${empHours} hours, Daily Wage: $${dailyWage}`);
+// Calculating total monthly wage
+let totalEmpHours = 0;
+
+for (let day = 0; day < WORKING_DAYS_PER_MONTH; day++) {
+    let empCheck = Math.floor(Math.random() * 3);
+    totalEmpHours += getWorkHours(empCheck);
+}
+
+let totalWage = totalEmpHours * WAGE_PER_HOUR;
+console.log(`Total Hours Worked: ${totalEmpHours}, Monthly Wage: $${totalWage}`);
