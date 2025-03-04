ff652a2d012840a4acdcda49a8a928726917f86e
Ashish151102 <ashishkumyadav789@gmail.com>
2025-03-04 12:50:54 +0530

    [Ashish] Add . Storing daily wages and calculating total wages.

--- src/main/javascript/com.bridgelabz/EmployeeWage.js
+++ src/main/javascript/com.bridgelabz/EmployeeWage.js
@@ -1,10 +1,10 @@
-const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const WORKING_DAYS_PER_MONTH = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
 
 // Function to get work hours
 function getWorkHours(empCheck) {
@@ -16,51 +16,27 @@ function getWorkHours(empCheck) {
         default:
             return 0;
     }
-}const IS_PART_TIME = 1;
- const IS_FULL_TIME = 2;
- const PART_TIME_HOURS = 4;
- const FULL_TIME_HOURS = 8;
- const WAGE_PER_HOUR = 20;
- const MAX_WORKING_DAYS = 20;
- const MAX_WORKING_HOURS = 160;
-
- // Function to get work hours
- function getWorkHours(empCheck) {
-     switch (empCheck) {
-         case IS_PART_TIME:
-             return PART_TIME_HOURS;
-         case IS_FULL_TIME:
-             return FULL_TIME_HOURS;
-         default:
-             return 0;
-     }
- }
-
- // Function for calculating total wages until limit is reached
- function calculateTotalWage() {
-     let totalEmpHours = 0;
-     let totalWorkingDays = 0;
-
-     while (totalEmpHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
-         let empCheck = Math.floor(Math.random() * 3);
-         totalEmpHours += getWorkHours(empCheck);
-         totalWorkingDays++;
-     }
-
-     return totalEmpHours * WAGE_PER_HOUR;
- }
-
- // calculating total wages
- let totalWage = calculateTotalWage();
- console.log(`Total Monthly Wage: $${totalWage}`);
-
-// Calculating total monthly wage
-let totalEmpHours = 0;
-
-for (let day = 0; day < WORKING_DAYS_PER_MONTH; day++) {
-    let empCheck = Math.floor(Math.random() * 3);
-    totalEmpHours += getWorkHours(empCheck);
 }
 
-let totalWage = totalEmpHours * WAGE_PER_HOUR;
-console.log(`Total Hours Worked: ${totalEmpHours}, Monthly Wage: $${totalWage}`);
+// Function for storing daily wages and calculating total wages
+function calculateTotalWage() {
+    let totalEmpHours = 0;
+    let totalWorkingDays = 0;
+    let dailyWages = [];
+
+    while (totalEmpHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+        let empCheck = Math.floor(Math.random() * 3);
+        let empHours = getWorkHours(empCheck);
+        let dailyWage = empHours * WAGE_PER_HOUR;
+
+        dailyWages.push(dailyWage); // Store daily wage
+        totalEmpHours += empHours;
+        totalWorkingDays++;
+    }
+
+    return totalEmpHours * WAGE_PER_HOUR;
+}
+
+// calculating total wages
+let totalWage = calculateTotalWage();
+console.log(`Total Monthly Wage: $${totalWage}`);
